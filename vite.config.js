7b33003341949bbce415798f264795a7a662b3c2
tadiwakabs <tadiwakabayadondo@gmail.com>
2025-09-22 09:31:14 -0500

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/portfolio/",
+    base: "",
   plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
